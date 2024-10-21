a62e9188b50b77ef7e0ef884d5b699afef9ad23a
kwaku1166 <bfrimpong@inop.ai>
2024-10-21 17:17:40 +0300

    added state machine to switch between state and will implement this tomorrow

--- robot_control/custom_behavior/behavior_state_machine.py
+++ robot_control/custom_behavior/behavior_state_machine.py
@@ -1,3 +1,11 @@
+# event parameters
+D_STOP = 0.05  # meters from goal
+D_CAUTION = 0.15  # meters from obstacle
+D_DANGER = 0.04  # meters from obstacle
+
+# progress margin
+PROGRESS_EPSILON = 0.05
+
 class BehaviorStateMachine:
     def __init__(self, supervisor) -> None:
-        pass
+        self.supervisor = supervisor
