c8c9a89a7ef20a8d59b2a4e664511cb5ed55f9c6
kwaku1166 <bfrimpong@inop.ai>
2024-10-23 01:34:02 +0300

    mercimek

--- robot_control/custom_behavior/behavior_state_machine.py
+++ robot_control/custom_behavior/behavior_state_machine.py
@@ -1,4 +1,7 @@
 # event parameters
+from robot_control.custom_behavior.swarm_behavior import SwarmBehavior
+
+
 D_STOP = 0.05  # meters from goal
 D_CAUTION = 0.15  # meters from obstacle
 D_DANGER = 0.04  # meters from obstacle
@@ -7,5 +10,21 @@ D_DANGER = 0.04  # meters from obstacle
 PROGRESS_EPSILON = 0.05
 
 class BehaviorStateMachine:
-    def __init__(self, supervisor) -> None:
+    def __init__(self, supervisor: SwarmBehavior) -> None:
         self.supervisor = supervisor
+
+        #set the initial state
+        self.transition_to_search_state()
+    
+    def update_state(self):
+        pass
+
+
+    def transition_to_search_state(self):
+        pass
+
+    def transition_to_wait_state(self):
+        pass
+
+    def transition_to_depart_swarm_state(self):
+        pass
